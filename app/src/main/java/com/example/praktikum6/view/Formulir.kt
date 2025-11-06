645ecd03069ea310c9b1cf00fbd6aa3d80b7f736
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-11-06 15:33:59 +0700

    tambahkan composable FormIsian untuk form isian pengguna

---
+++ app/src/main/java/com/example/praktikum6/view/Formulir.kt
@@ -0,0 +1,12 @@
+package com.example.praktikum6.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+
+
+}
